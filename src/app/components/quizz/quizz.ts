9f9889574f04d938c33ccf44150a348ba582167d
Ryan Ferreira <ryanfsilva6@gmail.com>
2025-09-03 14:20:13 -0300

    Finalização do clone BuzzFeed

---
+++ src/app/components/quizz/quizz.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { NgIf, NgForOf } from "@angular/common";
+import quizz_questions from '../../../assets/data/quizz_questions.json'
+
+@Component({
+  selector: 'app-quizz',
+  imports: [NgIf, NgForOf],
+  templateUrl: './quizz.html',
+  styleUrl: './quizz.css'
+})
+export class Quizz {
+
+	title:string = '';
+
+	questions:any
+	questionSelected:any
+
+	answers:string[] = []
+	answerSelected:string = ''
+
+	questionIndex:number = 0
+	questionMaxIndex:number = 0
+
+	finished:boolean = false
+
+	ngOnInit() {
+		if (quizz_questions) {
+			this.finished = false
+			this.title = quizz_questions.title;
+
+			this.questions = quizz_questions.questions;
+			this.questionSelected = this.questions[this.questionIndex]
+
+			this.questionIndex = 0
+			this.questionMaxIndex = this.questions.length
+
+			console.log(this.questionIndex)
+			console.log(this.questionMaxIndex)
+		}
+	}
+
+	playerChoose(value:string) {
+		this.answers.push(value)
+		this.nextStep()
+	}
+
+	 async nextStep() {
+		this.questionIndex +=1
+
+		if(this.questionMaxIndex > this.questionIndex){
+			this.questionSelected = this.questions[this.questionIndex]
+		} else {
+			const finalAnswer:string = await this.checkResult(this.answers)
+			this.finished = true;
+			this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
+		}
+	}
+
+	async checkResult(answers:string[]) {
+		const result = answers.reduce((previous, current, i, arr) => {
+			if(arr.filter(item => item === previous).length > arr.filter(item => item === current).length) {
+				return previous
+			} else {
+				return current
+			}
+		})
+
+		return result
+	}
+
+}
